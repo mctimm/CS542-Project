d10ec3de7adbda464fc4895cee1d4fa32e8200ed
Ryan Goodner <rgoodner@unm.edu>
2022-11-21 22:41:33 -0700

    update results

--- LocalAlltoall3.cpp
+++ LocalAlltoall3.cpp
@@ -272,7 +272,7 @@ int main(int argc, char *argv[]) {
     if (rank == 0) {
       end = get_time();
       // append time
-      printf("%s,%d,%d,%g\n", "RSM_Alltoall", num_procs, num_doubles,
+      printf("%s,%d,%d,%g\n", "AlltoallNoShiftBuffered", num_procs, num_doubles,
           (end - start) / num_measurements); // csv row
     }
 
