fd966b93bc069e9684d607cdf814db43951182b6
ryan goodner <ryangoodner@pm.me>
2022-11-27 06:11:56 -0700

    prep for 8x8 test

--- localAlltoall_8x8.sh
+++ localAlltoall_8x8.sh
@@ -5,12 +5,12 @@
 #SBATCH --ntasks 64
 #SBATCH --ntasks-per-node 8
 #SBATCH --mem 4G
-#SBATCH --time 00:10:00
+#SBATCH --time 00:30:00
 #SBATCH --job-name localAlltoall8x8
 
 . env.sh
 srun --mpi=pmi2 hostname
 echo 'algorithm,num_procs,num_doubles_per_proc,seconds' > results_8x8.csv
-srun -n 64 -N 8 ./build/LocalAlltoall >> results_8x8.csv
+srun -n 64 -N 8 ./build/LocalAlltoall2 >> results_8x8.csv
+srun -n 64 -N 8 ./build/LocalAlltoall3 >> results_8x8.csv
 srun -n 64 -N 8 ./build/Alltoall >> results_8x8.csv
-
