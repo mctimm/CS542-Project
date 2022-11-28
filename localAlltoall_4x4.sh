4c2f378028eb1630657392f2d9e1bcc70836fb4f
Ryan Goodner <rgoodner@unm.edu>
2022-11-28 14:20:43 -0700

    update 4x4 results

--- localAlltoall_4x4.sh
+++ localAlltoall_4x4.sh
@@ -10,9 +10,10 @@
 
 . env.sh
 srun --mpi=pmi2 hostname
-echo 'algorithm,num_procs,num_doubles_per_proc,seconds' > results_4x4.csv
-srun -n 16 -N 4 ./build/LocalAlltoall >> results_4x4.csv
-srun -n 16 -N 4 ./build/Alltoall >> results_4x4.csv
-srun -n 16 -N 4 ./build/LocalAlltoall2 >> results_4x4.csv
-srun -n 16 -N 4 ./build/LocalAlltoall3 >> results_4x4.csv
-srun -n 16 -N 4 ./build/LocalAlltoall_comm >> results_4x4.csv
+#echo 'algorithm,num_procs,num_doubles_per_proc,seconds' > results_4x4.csv
+#srun --mpi=pmi2 -n 16 -N 4 ./build/Alltoall >> results_4x4.csv
+#srun --mpi=pmi2 -n 16 -N 4 ./build/LocalAlltoall >> results_4x4.csv
+#srun --mpi=pmi2 -n 16 -N 4 ./build/LocalAlltoall_comm >> results_4x4.csv
+#srun --mpi=pmi2 -n 16 -N 4 ./build/LocalAlltoall2 >> results_4x4.csv
+srun --mpi=pmi2 -n 16 -N 4 ./build/LocalAlltoall3 >> results_4x4.csv
+
