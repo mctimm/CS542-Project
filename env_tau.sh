6648a1a9ed51172064dcd9fdc872599c9fa485a6
Ryan Goodner <rgoodner@unm.edu>
2022-12-08 10:33:12 -0700

    prep for jumpshot

--- env_tau.sh
+++ env_tau.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 module purge
 module load gcc cmake
-spack load openmpi
+# spack load openmpi
+eval `~/spack/bin/spack load --sh   openmpi` # not sure why spack stopped working normally
 export TAU_PROFILE=1
 export TAU_COMM_MATRIX=1
-export TAU_TRACE=0
+export TAU_TRACE=1
 
